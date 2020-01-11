acc1511ecd18650dcf1d80b2d20b0bddad8c73e9
Israel Martinez <iukones1@gmail.com>
2020-01-10 19:50:13 -0600

    refactor code

---
+++ README.md
@@ -0,0 +1,9 @@
+Iniciar proyecto en local
+============
+
+Dependencias y plugins instalados desde VSC
+
+necesario para correr los ejercicios. `Live Sass Compiler` y `Live Server`.
+
+  * Watch Sass
+  * Go Live
---
+++ custom-svg-checkbox/js/source.js
@@ -0,0 +1,10 @@
+let checkbox = document.getElementById('cb');
+    let svg = document.getElementById('checkbox');
+
+    svg.addEventListener('click', () => {
+      if (checkbox.checked) 
+        svg.classList.add('reverse')
+      else
+        svg.classList.remove('reverse')
+      
+    });
